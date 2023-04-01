a6ed00f2946a62fb785a6717fd1a39b2caa9197e
Zyrucen <116550262+Zyrucen@users.noreply.github.com>
2023-04-01 10:15:08 +0200

    Terminado

--- Program.cs
+++ Program.cs
@@ -3,10 +3,25 @@
 namespace ContarPalabras
 {
     internal class Program
+
+    /*  Este es un programa que pide al usuario una cadena de texto y
+     *  le muestra cuántas palabras hay en la cadena.
+     */
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string texto;
+
+            Console.WriteLine("Este programa está diseñado para contar las palabras.\n" +
+                "Por favor, escribe el texto que quieras:");
+
+            texto = Console.ReadLine();
+
+            string [] palabras = texto.Split(' ');
+
+            Console.WriteLine(palabras.Length);
+
         }
     }
 }
